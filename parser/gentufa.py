aaf605af8bec1a3d53b2de78f7e2a510cad5241b
André Madeira Cortes <amadeiracortes@gmail.com>
2023-07-27 00:45:26 +0200

    Add feature to augment PEG with tags

--- parser/gentufa.py
+++ parser/gentufa.py
@@ -1,9 +1,10 @@
 import os
+import re
 import sys
 
 from parsimonious.grammar import Grammar
 
-# from parsimonious.nodes import NodeVisitor
+from dictionary.jbovlaste import Jbovlaste
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -15,9 +16,15 @@ class Gentufa:
     """
 
     def __init__(self):
+        dictionary = Jbovlaste().get_whole_dict()
         with open(os.path.join(__location__, "gerna.peg"), "r") as f:
-        #with open(os.path.join(__location__, "camxes.peg"), "r") as f:
             grammar_text = f.read()
+        # Augment grammar text with dictionary words
+        tags = re.findall(r"\{\{(.*?)\}\}", grammar_text)
+        for tag in tags:
+            words = '" / "'.join(dictionary[tag].keys())
+            words = '"' + words + '"\n' # this join method doesn't add the initial double quotes
+            grammar_text = grammar_text.replace("{{{{{0}}}}}".format(tag), words) # replace tag with dictionary words
         self.grammar = Grammar(grammar_text)
 
     def get_parsed_sentence(self, sentence):
