7abb96a422ae0a80d3bed3863b22fb21533d5fb3
André Madeira Cortes <amadeiracortes@gmail.com>
2024-08-05 03:03:02 +0200

    Simplify visitor functions collection

--- visitor/visitor_functions/__init__.py
+++ visitor/visitor_functions/__init__.py
@@ -5,14 +5,12 @@ from inspect import getmembers, isfunction
 
 from . import base_grammar, selmaho
 
+functions = []
+
 base_grammar_functions = getmembers(base_grammar, isfunction)
-base_grammar_function_names = []
 selmaho_functions = getmembers(selmaho, isfunction)
-selmaho_function_names = []
 
 for function in base_grammar_functions:
-    base_grammar_function_names.append(function[1])
+    functions.append(function[1])
 for function in selmaho_functions:
-    selmaho_function_names.append(function[1])
-
-functions = tuple(base_grammar_function_names) + tuple(selmaho_function_names)
+    functions.append(function[1])
